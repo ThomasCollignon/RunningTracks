669251b7a792b7cf2edaf26748f2a8e9d035f5c7
Thomas Collignon <thom.collignon@gmail.com>
2023-03-22 11:48:50 +0100

    feat: logs

--- src/main/java/org/coli/routegenerator/RTController.java
+++ src/main/java/org/coli/routegenerator/RTController.java
@@ -1,6 +1,7 @@
 package org.coli.routegenerator;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import static java.lang.Integer.parseInt;
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
+@Slf4j
 public class RTController {
 
     private final PointsLoader pointsLoader;
@@ -22,6 +24,9 @@ public class RTController {
     @GetMapping("/")
     public RestModel go(@RequestParam(defaultValue = "10") String distanceKmString,
                         @RequestParam(defaultValue = "Chastre") String startingPoint) {
+        log.debug("Controller reached with"
+                          + "\n\tdistanceKmString = " + distanceKmString
+                          + "\n\tstartingPoint = " + startingPoint);
         final PointsMap pointsMap = startingPoint.equals("Libersart")
                                     ? pointsLoader.getPointsMapLibersart()
                                     : pointsLoader.getPointsMapChastre();
